2748ea1e46aaf7b72966206fc60fe49992efa125
Thamara Liyanage <thamara.prabuddha@gmail.com>
2024-10-14 20:11:51 +0530

    feat(neovim): add nvim-dap plugin

---
+++ home/dot_config/nvim/lua/exact_plugins/nvim-dap.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    'mfussenegger/nvim-dap',
+    config = function()
+      local dap = require 'dap'
+      vim.keymap.set('n', '<Leader>dt', dap.toggle_breakpoint, {})
+      vim.keymap.set('n', '<Leader>dc', dap.continue, {})
+    end,
+  },
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
+    config = function()
+      local dap, dapui = require 'dap', require 'dapui'
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end,
+  },
+}
