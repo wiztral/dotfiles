9e4c2fb26fc34fccbe8d02adea736a1f333c598e
Thamara Liyanage <thamara.prabuddha@gmail.com>
2024-10-17 21:58:25 +0530

    feat(nvim): rename tsserver to ts_ls

--- home/dot_config/nvim/lua/exact_plugins/lsp-config.lua
+++ home/dot_config/nvim/lua/exact_plugins/lsp-config.lua
@@ -146,7 +146,7 @@ return { -- LSP Configuration & Plugins
       --    https://github.com/pmizio/typescript-tools.nvim
       --
       -- But for many setups, the LSP (`tsserver`) will work just fine
-      tsserver = {},
+      ts_ls = {},
       eslint = {
         on_new_config = function(config, new_root_dir)
           -- The "workspaceFolder" is a VSCode concept. It limits how far the
