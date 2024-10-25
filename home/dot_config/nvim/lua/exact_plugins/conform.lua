c3b17cd9312ee35c2cd2b2f48adf96f1cf59f593
Thamara Liyanage <thamara.prabuddha@gmail.com>
2024-10-25 23:01:13 +0530

    refactor(nvim): organize formatter related config

--- home/dot_config/nvim/lua/exact_plugins/conform.lua
+++ home/dot_config/nvim/lua/exact_plugins/conform.lua
@@ -2,6 +2,9 @@ local slow_format_filetypes = {}
 local disable_filetypes = { c = true, cpp = true }
 return { -- Autoformat
   'stevearc/conform.nvim',
+  dependencies = {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+  },
   event = { 'BufWritePre' },
   cmd = { 'ConformInfo' },
   keys = {
@@ -14,45 +17,57 @@ return { -- Autoformat
       desc = '[F]ormat buffer',
     },
   },
-  opts = {
-    notify_on_error = false,
-    format_on_save = function(bufnr)
-      if slow_format_filetypes[vim.bo[bufnr].filetype] then
-        return
-      end
-      local function on_format(err)
-        if err and err:match 'timeout$' then
-          slow_format_filetypes[vim.bo[bufnr].filetype] = true
-        end
-      end
+  opts = function()
+    local ensure_installed = {
+      'stylua', -- Used to format Lua code
+      'prettierd',
+      'prettier',
+      'ktfmt',
+      'google-java-format',
+    }
 
-      return {
-        timeout_ms = 200,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-      }, on_format
-    end,
-    format_after_save = function(bufnr)
-      if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-        return
-      end
-      return {
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-      }
-    end,
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      javascript = { 'prettierd' },
-      javascriptreact = { 'prettierd' },
-      typescript = { 'prettierd' },
-      typescriptreact = { 'prettierd' },
-      json = { 'prettierd' },
-      html = { 'prettierd' },
-      css = { 'prettierd' },
-      kotlin = { 'ktfmt' },
-      java = { 'google-java-format' },
-      -- python = { 'isort', 'black' },
-    },
-  },
+    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+    return {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        if slow_format_filetypes[vim.bo[bufnr].filetype] then
+          return
+        end
+        local function on_format(err)
+          if err and err:match 'timeout$' then
+            slow_format_filetypes[vim.bo[bufnr].filetype] = true
+          end
+        end
+
+        return {
+          timeout_ms = 200,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }, on_format
+      end,
+      format_after_save = function(bufnr)
+        if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+          return
+        end
+        return {
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        javascript = { 'prettierd' },
+        javascriptreact = { 'prettierd' },
+        typescript = { 'prettierd' },
+        typescriptreact = { 'prettierd' },
+        json = { 'prettierd' },
+        html = { 'prettierd' },
+        css = { 'prettierd' },
+        kotlin = { 'ktfmt' },
+        java = { 'google-java-format' },
+        -- python = { 'isort', 'black' },
+      },
+    }
+  end,
   init = function()
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
   end,
