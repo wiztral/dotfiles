1f1ef89224a16f4ed77543d3432c23297d6a2ffd
Thamara Liyanage <thamara.prabuddha@gmail.com>
2025-04-06 14:44:50 +0530

    feat(nvim): switch to customized catppuccin colorscheme

---
+++ home/dot_config/exact_nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    opts = {
+      flavour = "mocha",
+      color_overrides = {
+        mocha = {
+          surface2 = "#4F4F5F",
+          surface1 = "#282836",
+          surface0 = "#15151F",
+          base = "#0B0B12",
+          mantle = "#06060A",
+          crust = "#020203",
+        },
+      },
+    },
+  },
+}
