ce7da618390b6a021c11a7fb31efd4d373bb962d
Thamara Liyanage <thamara.prabuddha@gmail.com>
2025-08-21 00:52:21 +0530

    feat(PowerShell): replace starship with oh-my-posh

--- home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -8,27 +8,4 @@ Set-PsFzfOption -TabExpansion
 
 fnm env --use-on-cd | Out-String | Invoke-Expression
 
-$prompt = ""
-function Invoke-Starship-PreCommand {
-    $current_location = $executionContext.SessionState.Path.CurrentLocation
-
-    if ($current_location.Provider.Name -eq "FileSystem") {
-        $ansi_escape = [char]27
-        $provider_path = $current_location.ProviderPath -replace "\\", "/"
-        $prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
-    }
-
-    $host.ui.Write($prompt)
-}
-
-Invoke-Expression (&starship init powershell)
-
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
+oh-my-posh init pwsh --config ~/.config/ohmyposh/base.toml | Invoke-Expression
