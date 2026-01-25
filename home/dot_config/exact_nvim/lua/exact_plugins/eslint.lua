2b81426713b8d2b0fcefc09a7e17ad4cf661129a
Thamara Liyanage <thamara.prabuddha@gmail.com>
2026-01-25 10:10:35 +0530

    feat(nvim): update eslint plugin config

--- home/dot_config/exact_nvim/lua/exact_plugins/eslint.lua
+++ home/dot_config/exact_nvim/lua/exact_plugins/eslint.lua
@@ -3,13 +3,17 @@ return {
   opts = {
     servers = {
       eslint = {
-        on_new_config = function(config, new_root_dir)
-          -- Help eslint find the correct workspace folder
-          config.settings.workspaceFolder = {
-            uri = vim.uri_from_fname(new_root_dir),
-            name = vim.fn.fnamemodify(new_root_dir, ":t"),
-          }
-        end,
+        settings = {
+          -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+          workingDirectories = { mode = "location" },
+        },
+        -- on_new_config = function(config, new_root_dir)
+        --   -- Help eslint find the correct workspace folder
+        --   config.settings.workspaceFolder = {
+        --     uri = vim.uri_from_fname(new_root_dir),
+        --     name = vim.fn.fnamemodify(new_root_dir, ":t"),
+        --   }
+        -- end,
       },
     },
   },
